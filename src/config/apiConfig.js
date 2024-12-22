ab031b34c134608897dfa66109816ec853bec594
tdv <tuduongvan2305@gmail.com>
2024-12-22 20:13:26 +0700

    f

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,10 +1,10 @@
 export const API_ENDPOINTS = {
-  BASE_URL: 'https://librarybe-f7dpbmd5fte9ggd7.southeastasia-01.azurewebsites.net', // hoặc 'http://localhost:8080' tùy vào môi trường 'http://localhost:8080' tùy vào môi trường
+  BASE_URL: process.env.REACT_APP_BASE_URL || 'http://localhost:8080', // Mặc định dùng localhost nếu không có biến môi trường
   USER: {
     BOOKS: '/books/categories',
     BOOK: '/books',
-    SUGGESTBOOK:'/books/suggest',
-    CATEGORYBOOK:'/books/categories'
+    SUGGESTBOOK: '/books/suggest',
+    CATEGORYBOOK: '/books/categories',
   },
   ADMIN: {
     BOOKS: '/books',
@@ -16,10 +16,10 @@ export const API_ENDPOINTS = {
     ADDMEM: '/members/register',
     EDITMEMBER: '/members/update',
     DELETEMEM: '/members/delete',
-    POSTS: '/posts', // Lấy tất cả bài viết
-    UPDATEPOSTS: '/posts/update', // Cập nhật bài viết
+    POSTS: '/posts',
+    UPDATEPOSTS: '/posts/update',
     ADDPOSTS: '/posts',
-    DELETEPOSTS: '/posts/delete',// Xóa bài viết theo id
+    DELETEPOSTS: '/posts/delete',
     BORROWED: '/transactions/borrowed',
     ADDBORROWED: '/transactions/borrow',
     ADDRETURN: '/transactions/return',
@@ -31,7 +31,7 @@ export const API_ENDPOINTS = {
   STATISTICS: {
     CATEGORY: '/books/category-distribution',
   },
-  TRANSACTIONS:{
-    BORROW:'/transactions/borrow'
-  } 
+  TRANSACTIONS: {
+    BORROW: '/transactions/borrow',
+  },
 };
